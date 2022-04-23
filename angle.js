fcf989ecfe749b74a6e77212b40cc8262fae91a7
Mir <mirshazilfaisal@ku.edu>
2022-04-22 21:15:13 -0500

    Added angle.js

---
+++ angle.js
@@ -0,0 +1,8 @@
+let angle = document.querySelector('#angle')
+
+angle.addEventListener("click", ()=> {
+    lbutton.classList.remove("show")
+    button.style.display = "none"
+    changed.innerText = "Kmh/Mph"
+})
+
